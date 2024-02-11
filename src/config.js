bd8060907653578febb18ae7b72afb8a98430df8
dmswo.lee <lej10048207@outlook.kr>
2024-02-11 20:35:35 +0900

    delete(main branch): directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "owners": ["자기 아이디"],
-    "timezone": "Asia/Seoul"
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-const CustomClient = require("./classes/CustomClient");
-const client = new CustomClient();
-
-client.start();
